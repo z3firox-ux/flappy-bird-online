3a23095c66c1967ab09350faaf8d4d37c83d1694
Flavio Demian Gonzalez <130401171+zLunarr@users.noreply.github.com>
2026-02-16 15:03:49 -0300

    Fix restart lifecycle to keep shared SpriteBatch alive

--- core/src/com/xili7/game/MyGdxGame.java
+++ core/src/com/xili7/game/MyGdxGame.java
@@ -1,14 +1,36 @@
 package com.xili7.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
 /**
  * Created by liray on 12/11/2015.
  */
 public class MyGdxGame extends Game {
+    private SpriteBatch batch;
+    private BitmapFont font;
+
     @Override
     public void create() {
-        setScreen(new MainScreen());
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        setScreen(new MainScreen(this));
+    }
+
+    public SpriteBatch getBatch() {
+        return batch;
+    }
+
+    public BitmapFont getFont() {
+        return font;
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        font.dispose();
+        batch.dispose();
     }
 }
