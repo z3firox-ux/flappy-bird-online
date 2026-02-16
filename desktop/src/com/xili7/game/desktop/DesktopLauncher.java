dff606e00d79b904ec4e50f87beb6fc6e6493e88
Flavio Demian Gonzalez <130401171+zLunarr@users.noreply.github.com>
2026-02-16 13:30:03 -0300

    Modernize Gradle/LibGDX build for Java 17 and Gradle 8

--- desktop/src/com/xili7/game/desktop/DesktopLauncher.java
+++ desktop/src/com/xili7/game/desktop/DesktopLauncher.java
@@ -1,12 +1,17 @@
 package com.xili7.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.xili7.game.MyGdxGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
-	}
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Flappy Bird");
+        config.setWindowedMode(480, 800);
+        config.useVsync(true);
+        config.setForegroundFPS(60);
+
+        new Lwjgl3Application(new MyGdxGame(), config);
+    }
 }
