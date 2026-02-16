4db10f40f169450ea9a9e09d28be7c5e99a0ee78
arenastiago30-dev <arenastiago3.0@gmail.com>
2026-02-16 16:57:29 -0300

    Update main menu to use sprite-based UI elements

--- core/src/com/xili7/game/MainMenuScreen.java
+++ core/src/com/xili7/game/MainMenuScreen.java
@@ -4,18 +4,29 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MainMenuScreen implements Screen {
     private final MyGdxGame game;
     private Stage stage;
     private Skin skin;
+
+    private Texture backgroundTexture;
+    private Texture titleTexture;
+    private Texture playTexture;
+    private Texture onlineTexture;
+    private Texture optionsTexture;
 
     public MainMenuScreen(MyGdxGame game) {
         this.game = game;
@@ -28,14 +39,29 @@ public class MainMenuScreen implements Screen {
         stage = new Stage(new ScreenViewport(), game.getBatch());
         skin = UiSkinFactory.createDefaultSkin();
 
+        backgroundTexture = new Texture("png/stage_sky.png");
+        titleTexture = new Texture("png/title.png");
+        playTexture = new Texture("png/playbuttom.png");
+        onlineTexture = new Texture("png/online.png");
+        optionsTexture = new Texture("png/options.png");
+
+        Image background = new Image(backgroundTexture);
+        background.setFillParent(true);
+        background.setScaling(Scaling.fill);
+        stage.addActor(background);
+
         Table root = new Table();
         root.setFillParent(true);
-        root.defaults().pad(12f).width(240f);
+        root.center();
+        root.defaults().pad(10f);
 
-        Label title = new Label("FLAPPY BIRD", skin);
-        TextButton playButton = new TextButton("PLAY", skin);
-        TextButton onlineButton = new TextButton("ONLINE", skin);
-        TextButton optionsButton = new TextButton("OPTIONS", skin);
+        Image title = new Image(titleTexture);
+        title.setScaling(Scaling.fit);
+
+        ImageButton playButton = createMenuButton(playTexture);
+        ImageButton onlineButton = createMenuButton(onlineTexture);
+        ImageButton optionsButton = createMenuButton(optionsTexture);
+        Label onlineStatus = new Label("", skin);
 
         playButton.addListener(new ChangeListener() {
             @Override
@@ -47,7 +73,7 @@ public class MainMenuScreen implements Screen {
         onlineButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, com.badlogic.gdx.scenes.scene2d.Actor actor) {
-                onlineButton.setText("ONLINE (COMING SOON)");
+                onlineStatus.setText("ONLINE (COMING SOON)");
             }
         });
 
@@ -63,13 +89,25 @@ public class MainMenuScreen implements Screen {
             }
         });
 
-        root.add(title).padBottom(32f).row();
-        root.add(playButton).row();
-        root.add(onlineButton).row();
-        root.add(optionsButton).row();
+        root.add(title)
+            .size(titleTexture.getWidth(), titleTexture.getHeight())
+            .padBottom(26f)
+            .row();
+        root.add(playButton).size(playTexture.getWidth(), playTexture.getHeight()).row();
+        root.add(onlineButton).size(onlineTexture.getWidth(), onlineTexture.getHeight()).row();
+        root.add(optionsButton).size(optionsTexture.getWidth(), optionsTexture.getHeight()).row();
+        root.add(onlineStatus).padTop(8f).row();
 
         stage.addActor(root);
         Gdx.input.setInputProcessor(stage);
+    }
+
+    private ImageButton createMenuButton(Texture texture) {
+        TextureRegionDrawable drawable = new TextureRegionDrawable(new TextureRegion(texture));
+        ImageButton.ImageButtonStyle style = new ImageButton.ImageButtonStyle();
+        style.imageUp = drawable;
+        style.imageDown = drawable;
+        return new ImageButton(style);
     }
 
     @Override
@@ -79,7 +117,7 @@ public class MainMenuScreen implements Screen {
             return;
         }
 
-        Gdx.gl.glClearColor(0.05f, 0.05f, 0.08f, 1f);
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         stage.act(delta);
@@ -111,6 +149,21 @@ public class MainMenuScreen implements Screen {
         }
         if (skin != null) {
             skin.dispose();
+        }
+        if (backgroundTexture != null) {
+            backgroundTexture.dispose();
+        }
+        if (titleTexture != null) {
+            titleTexture.dispose();
+        }
+        if (playTexture != null) {
+            playTexture.dispose();
+        }
+        if (onlineTexture != null) {
+            onlineTexture.dispose();
+        }
+        if (optionsTexture != null) {
+            optionsTexture.dispose();
         }
     }
 }
