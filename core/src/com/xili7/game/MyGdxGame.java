ac2139b3a09863b048942208155422dbf4bfa635
Flavio Demian Gonzalez <130401171+zLunarr@users.noreply.github.com>
2026-02-16 15:32:02 -0300

    Remove unsupported audio binary and use runtime music fallback

--- core/src/com/xili7/game/MyGdxGame.java
+++ core/src/com/xili7/game/MyGdxGame.java
@@ -1,6 +1,9 @@
 package com.xili7.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -11,11 +14,22 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGdxGame extends Game {
     private SpriteBatch batch;
     private BitmapFont font;
+    private Music backgroundMusic;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         font = new BitmapFont();
+
+        FileHandle preferredMusic = Gdx.files.internal("sound/Juego35.wav");
+        FileHandle fallbackMusic = Gdx.files.internal("png/Juego 35.wav");
+        FileHandle selectedMusic = preferredMusic.exists() ? preferredMusic : fallbackMusic;
+
+        backgroundMusic = Gdx.audio.newMusic(selectedMusic);
+        backgroundMusic.setLooping(true);
+        backgroundMusic.setVolume(0.5f);
+        backgroundMusic.play();
+
         setScreen(new MainScreen(this));
     }
 
@@ -30,6 +44,12 @@ public class MyGdxGame extends Game {
     @Override
     public void dispose() {
         super.dispose();
+        if (backgroundMusic != null) {
+            backgroundMusic.stop();
+            backgroundMusic.dispose();
+            backgroundMusic = null;
+        }
+
         font.dispose();
         batch.dispose();
     }
